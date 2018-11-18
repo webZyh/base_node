f67dad6c7608a7842fcecc946831eb3d14ca3678
zyh <jt_zyh10000@163.com>
2018-11-18 17:57:27 +0800

    用户评价

--- nodejs/blog项目/route/admin/index.js
+++ nodejs/blog项目/route/admin/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const urlLib = require('url');
 
 module.exports = function () {
     let router = express.Router();
     //检查登录状态
     router.use((req,res,next)=>{    //处理访问任何目录
+        //阻止 favicon.ico 的请求
+        /*const obj = urlLib.parse(req.url, true);
+        const pathname = obj.pathname;*/
+
         if (!req.session['admin_id'] && req.url!='/login'){ //没有登录
             res.redirect('/admin/login')
         }else{
@@ -15,8 +20,10 @@ module.exports = function () {
     router.get('/',(req,res)=>{
         res.render('admin/index.ejs',{})
     });
-
-    router.use('/banner',require('./banner')())
+    //banner路由
+    router.use('/banner',require('./banner')());
+    //用户评价路由
+    router.use('/custom',require('./custom')());
 
     return router;
 }
